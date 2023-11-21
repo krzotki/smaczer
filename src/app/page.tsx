bfc0dfdd106b3ebcbb432ef88511fc2d9c5c8832
krzotki <krzotki@gmail.com>
2023-11-21 21:23:26 +0100

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { RecipesList } from "@/components/RecipesList";
 import { gerRandomRecipesForWeek } from "@/recipes/getRecipes";
 import { Box, Flex, Headline, TextBit } from "brainly-style-guide";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const recipes = await gerRandomRecipesForWeek(9);
   return (
