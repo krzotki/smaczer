87be42fc675eb367ea1580a1aed1cb1517a46f01
Karol Rzotki <krzoti@gmail.com>
2023-11-25 17:49:33 +0100

    debug

--- src/recipes/rollRecipes.ts
+++ src/recipes/rollRecipes.ts
@@ -16,91 +16,92 @@ const rng = new RandomLCG(Date.now());
 export const rollWeeklyRecipes = () => {
   return new Promise((resolve, reject) => {
     MongoClient.connect(dbUrl)
-      .then(async (client) => {
+      .then((client) => {
+        console.log("Connected");
         const db = client.db(dbName);
-        console.log('Connected')
-        client.close()
-        return
-        const collections = await db.listCollections().toArray();
-        const collectionNames = collections.map((c) => c.name);
-        const collectionName = COLLECTION_WEEKLY_RECIPES;
-        if (!collectionNames.includes(collectionName)) {
-          await db.createCollection(collectionName);
-          console.log(`Collection ${collectionName} created`);
-        } else {
-          console.log(
-            `Collection ${collectionName} already exists. Clearing (droping and recreating) collection`
-          );
-          await db.dropCollection(collectionName);
-          await db.createCollection(collectionName);
-        }
+        console.log("Connected to db");
+        client.close();
+        return;
+        // const collections = await db.listCollections().toArray();
+        // const collectionNames = collections.map((c) => c.name);
+        // const collectionName = COLLECTION_WEEKLY_RECIPES;
+        // if (!collectionNames.includes(collectionName)) {
+        //   await db.createCollection(collectionName);
+        //   console.log(`Collection ${collectionName} created`);
+        // } else {
+        //   console.log(
+        //     `Collection ${collectionName} already exists. Clearing (droping and recreating) collection`
+        //   );
+        //   await db.dropCollection(collectionName);
+        //   await db.createCollection(collectionName);
+        // }
 
-        const recipes = await new Promise<Array<RecipeType>>(
-          (resolve, reject) => {
-            db.collection(COLLECTION_ALL_RECIPES)
-              .find({})
-              .toArray()
-              .then((items) => {
-                resolve(
-                  items.map(({ _id, ...rest }) => ({
-                    ...(rest as RecipeType),
-                    _id: _id.toString(),
-                  }))
-                );
-              })
-              .catch((error) => {
-                reject(error);
-              });
-          }
-        );
+        // const recipes = await new Promise<Array<RecipeType>>(
+        //   (resolve, reject) => {
+        //     db.collection(COLLECTION_ALL_RECIPES)
+        //       .find({})
+        //       .toArray()
+        //       .then((items) => {
+        //         resolve(
+        //           items.map(({ _id, ...rest }) => ({
+        //             ...(rest as RecipeType),
+        //             _id: _id.toString(),
+        //           }))
+        //         );
+        //       })
+        //       .catch((error) => {
+        //         reject(error);
+        //       });
+        //   }
+        // );
 
-        const randomRecipes = rng.pickRandomElements(recipes, 10);
-        const withCost = await Promise.all(
-          randomRecipes.map(async (recipe) => {
-            console.log({ recipe });
-            if (recipe.ingredientsCost) {
-              return recipe;
-            }
+        // const randomRecipes = rng.pickRandomElements(recipes, 10);
+        // const withCost = await Promise.all(
+        //   randomRecipes.map(async (recipe) => {
+        //     console.log({ recipe });
+        //     if (recipe.ingredientsCost) {
+        //       return recipe;
+        //     }
 
-            const cost = await getIngredientsPrice(ingredientsToString(recipe));
+        //     const cost = await getIngredientsPrice(ingredientsToString(recipe));
 
-            const updateRes = await db
-              .collection(COLLECTION_ALL_RECIPES)
-              .updateOne(
-                {
-                  _id: new ObjectId(recipe._id),
-                },
-                {
-                  $set: {
-                    ingredientsCost: cost,
-                  },
-                }
-              );
+        //     const updateRes = await db
+        //       .collection(COLLECTION_ALL_RECIPES)
+        //       .updateOne(
+        //         {
+        //           _id: new ObjectId(recipe._id),
+        //         },
+        //         {
+        //           $set: {
+        //             ingredientsCost: cost,
+        //           },
+        //         }
+        //       );
 
-            console.log({ updateRes })
+        //     console.log({ updateRes });
 
-            return {
-              ...recipe,
-              ingredientsCost: cost,
-            };
-          })
-        );
+        //     return {
+        //       ...recipe,
+        //       ingredientsCost: cost,
+        //     };
+        //   })
+        // );
 
-        db.collection(collectionName)
-          .insertMany(
-            withCost.map((recipe) => ({
-              ...recipe,
-              _id: new ObjectId(recipe._id),
-            }))
-          )
-          .then((result) => {
-            resolve(result);
-            client.close();
-          })
-          .catch((error) => {
-            reject(error);
-            client.close();
-          });
+        // db.collection(collectionName)
+        //   .insertMany(
+        //     withCost.map((recipe) => ({
+        //       ...recipe,
+        //       _id: new ObjectId(recipe._id),
+        //     }))
+        //   )
+        //   .then((result) => {
+        //     resolve(result);
+        //     client.close();
+        //   })
+        //   .catch((error) => {
+        //     reject(error);
+        //     client.close();
+        //   });
       })
       .catch((error) => {
         reject(error);
