ab9e90199aa7793f6ef5fb00550cd8f4a7e6e178
Karol Rzotki <krzoti@gmail.com>
2023-11-29 18:59:45 +0100

    dynamic title

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,9 +1,25 @@
 import { Recipe as RecipeView } from "@/components/Recipe";
 import { getRecipe } from "@/recipes/getRecipes";
 import { Flex, Headline } from "brainly-style-guide";
+import Head from "next/head";
 import Image from "next/image";
 
 export default async function Recipe({ params }: { params: { id: string } }) {
   const recipe = await getRecipe(params.id);
-  return <RecipeView recipe={recipe} />;
+  return (
+    <div>
+      <Head>
+        <title>{recipe.name}</title>
+      </Head>
+      <RecipeView recipe={recipe} />
+    </div>
+  );
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const recipe = await getRecipe(params.id);
+  return {
+    title: recipe.name,
+    description: recipe.description,
+  };
 }
