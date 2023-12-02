b056017df9c436b24638f2c3f8f309a668ea755f
Karol Rzotki <krzoti@gmail.com>
2023-12-02 18:33:41 +0100

    add allowed origins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        '192.168.1.11:3000'
+      ]
+    }
+  },
   images: {
     remotePatterns: [
       {
